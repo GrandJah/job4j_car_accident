6c5decd4c147519843aa9a1596376754f5f8697d
CrandJah <bujhtitxrf@gmail.com>
2021-01-15 03:13:35 +1100

    task -> 1. Загрузка данных в вид [#422709]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class IndexControl {
   @GetMapping("/")
   public String index(Model model) {
+    model.addAttribute("list", randList(30));
     return "index";
+  }
+
+  private List<String> randList(int max) {
+    return IntStream.range(0, max).filter((i) -> 0.3 <= Math.random())
+                    .mapToObj(i -> "generate row with random data i=" + i)
+                    .collect(Collectors.toList());
   }
 }
